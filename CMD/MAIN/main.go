032db3b87cde36f2ee4b2d20b08667f01de6b927
unknown <ganorkarmohit551@gmail.com>
2025-07-06 17:15:26 +0530

    Env-Variable Loading is done

---
+++ CMD/MAIN/main.go
@@ -0,0 +1,12 @@
+package Main
+
+import (
+	"github.com/FACELESS-GOD/Fitness-Exercise-Backend.git/Packages/Helper/ConfigSetup"
+	"github.com/FACELESS-GOD/Fitness-Exercise-Backend.git/Packages/Helper/FieldName"
+)
+
+func main() {
+
+	ConfigSetup.InitateSetup(FieldName.Dev)
+
+}
